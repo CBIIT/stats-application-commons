70b90bf7a1591493cb26e95714ac038b44a53d76
Harris Michael <harrismic>
2006-02-10 18:42:14 +0000

    Now implements the Application Service interface.
    SVN-Revision: 8086

--- src/gov/nih/nci/caintegrator/application/service/annotation/GeneExprAnnotationService.java
+++ src/gov/nih/nci/caintegrator/application/service/annotation/GeneExprAnnotationService.java
@@ -11,7 +11,7 @@ import java.util.Map;
  *
  */
 
-public abstract class GeneExprAnnotationService extends ApplicationService {
+public abstract class GeneExprAnnotationService implements ApplicationService {
 
 	public abstract GeneExprAnnotationService getInstance();
 	
