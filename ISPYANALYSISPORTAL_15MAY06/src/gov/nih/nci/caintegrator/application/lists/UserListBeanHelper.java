41b5802d01fc466e85f688c3cb2b0b198c6e9798
no_author <no_author@no_author.cvs2svn>
2006-05-15 16:18:36 +0000

    This commit was manufactured by cvs2svn to create tag 'ISPYANALYSISPORTAL_15MAY06'.
    SVN-Revision: 8135

---
+++ ISPYANALYSISPORTAL_15MAY06/src/gov/nih/nci/caintegrator/application/lists/UserListBeanHelper.java
@@ -0,0 +1,36 @@
+/**
+ * This helper class access the userListBean in the session. 
+ */
+package gov.nih.nci.caintegrator.application.lists;
+
+import java.util.List;
+
+import org.w3c.dom.Document;
+
+
+
+/**
+ * @author rossok
+ *
+ */
+public interface UserListBeanHelper {
+
+    public void addList(UserList userList);
+    
+    public void removeList(String listName);
+    
+    public void addItemToList(String listName, String listItem);
+    
+    public void removeItemFromList(String listName, String listItem);
+    
+    public Object getDetailsFromList(String listName);
+    
+    public List<UserList> getLists(ListType listType);
+    
+    public List<UserList> getAllLists();
+    
+    public UserList getUserList(String listName);
+    
+    public List getItemsFromList(String listName);
+
+}
