b139f032d2227f8c09ee8a3fb12d201ddfc0aec0
no_author <no_author@no_author.cvs2svn>
2006-05-30 14:37:38 +0000

    This commit was manufactured by cvs2svn to create tag 'ISPY-ANALYSIS-PORTAL_DEV_01JUNE06'.
    SVN-Revision: 8144

---
+++ ISPY-ANALYSIS-PORTAL_DEV_01JUNE06/src/gov/nih/nci/caintegrator/application/service/annotation/GeneExprAnnotationService.java
@@ -0,0 +1,22 @@
+package gov.nih.nci.caintegrator.application.service.annotation;
+
+import gov.nih.nci.caintegrator.application.service.ApplicationService;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This interface can be implemented by applications
+ * to provide an annotation service.
+ * @author harrismic
+ *
+ */
+
+public abstract class GeneExprAnnotationService implements ApplicationService {
+
+	public static GeneExprAnnotationService getInstance() { return null; }
+	
+	public abstract Map <String,ReporterAnnotation> getAnnotationsMapForReporters(List<String> reporterIDs) throws Exception;
+
+	public abstract List <ReporterAnnotation> getAnnotationsListForReporters(List<String> reporterIDs) throws Exception;
+
+}
