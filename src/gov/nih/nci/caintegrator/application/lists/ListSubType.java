afad053f4dc596e13c44debae0136733384db4db
Ryan Landy <landyr>
2006-07-10 18:29:41 +0000

    *** empty log message ***
    SVN-Revision: 8172

--- src/gov/nih/nci/caintegrator/application/lists/ListSubType.java
+++ src/gov/nih/nci/caintegrator/application/lists/ListSubType.java
@@ -1,6 +1,6 @@
 package gov.nih.nci.caintegrator.application.lists;
 
 public enum ListSubType {
-    Default, Custom, IMAGE_CLONE, PROBE_SET;
+    Default, Custom, IMAGE_CLONE, PROBE_SET, SNPProbeSet, DBSNP, GENBANK_ACCESSION_NUMBER, GENESYMBOL, LOCUS_LINK;
 }
 
