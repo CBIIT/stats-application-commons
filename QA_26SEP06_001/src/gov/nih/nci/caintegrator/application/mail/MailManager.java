f4b9f8350d201145c5b093fde0a86484ee9cbd31
no_author <no_author@no_author.cvs2svn>
2006-09-21 14:40:58 +0000

    This commit was manufactured by cvs2svn to create tag 'QA_26SEP06_001'.
    SVN-Revision: 8211

---
+++ QA_26SEP06_001/src/gov/nih/nci/caintegrator/application/mail/MailManager.java
@@ -0,0 +1,149 @@
+package gov.nih.nci.caintegrator.application.mail;
+
+import gov.nih.nci.caintegrator.exceptions.ValidationException;
+
+import java.io.File;
+import java.text.MessageFormat;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ *  Manages sending of emails 
+ *
+ */
+public class MailManager {
+	
+	private static Logger logger = Logger.getLogger(MailManager.class);
+
+	/**
+	 * Sends an email notifying user that their files are ready for download
+	 * via FTP
+	 * 
+	 * @param mailTo - email address
+	 * @param fileName - filename
+	 * @throws Exception
+	 */
+	public static void sendFTPMail(String mailTo, List<String> fileNames, String additionalText)
+	{
+		try
+		{				
+		    // Part 1 is always included  
+		    String message = new MessageFormat(MailConfig.getFtpUnformattedBody1()).format(new String[] {MailConfig.getFileRetentionPeriodInDays(),MailConfig.getProject(),MailConfig.getAcronym()});		  
+			
+		    // If there was additional text passed in then include that here
+		    if (additionalText != null)
+		    	message += additionalText + "\n\n";
+		    
+		    // Part 2 is only included if there are multiple fies
+		    if(fileNames.size() > 1) {
+		    	message += MailConfig.getFtpUnformattedBody2();	
+		    }
+		    	
+		    // Part 3 appears once for each file
+		    for(String fileName : fileNames)
+		    {    	
+		    	message += new MessageFormat(MailConfig.getFtpUnformattedBody3()).format(new String[] {cleanFileName(fileName), MailConfig.getFtpHostnameAndPort()});	
+		    }
+		    
+		    // Part 4 always appears  
+		    message += new MessageFormat(MailConfig.getFtpUnformattedBody4()).format(new String[] {MailConfig.getAppSupportNumber(), MailConfig.getTechSupportStartTime(), MailConfig.getTechSupportEndTime(),MailConfig.getAcronym(),MailConfig.getTechSupportURL()});
+		    
+	        // Send the message
+	        new SendMail().sendMail(mailTo, null, message,formatFTPSubject());		
+		} catch (Exception e) {
+			logger.error("Send FTP mail error", e);
+		} catch (ValidationException e) {
+			logger.error("Send FTP mail error", e);
+		}
+	 }
+	
+	/**
+	 * sendFTPErrorMail is used to send a message to the user and copy the help desk when
+	 * a query was not able to successfully complete.
+	 * <P>
+	 * @param mailTo The email address of the user to be notified
+	 */
+	public static void sendFTPErrorMail(String mailTo)
+	{
+		try
+		{				
+		    // Part 1 is always included  
+		    String message = new MessageFormat(MailConfig.getFtpUnformattedErrorBody1()).format(new String[] {MailConfig.getFileRetentionPeriodInDays(),MailConfig.getProject(),MailConfig.getAcronym()});		  
+		    
+		    // Part 2 always appears  
+		    message += new MessageFormat(MailConfig.getFtpUnformattedErrorBody2()).format(new String[] {MailConfig.getAppSupportNumber(), MailConfig.getTechSupportStartTime(), MailConfig.getTechSupportEndTime(),MailConfig.getAcronym(),MailConfig.getTechSupportURL()});
+		    
+	        // Send the message
+		    String mailCC = MailConfig.getTechSupportMail();
+		    
+	        new SendMail().sendMail(mailTo, mailCC, message,formatFTPErrorSubject());		
+		}
+		catch (Exception e)
+		{
+			logger.error("Send FTP mail error", e);
+		}
+		catch (ValidationException e)
+		{
+			logger.error("Send FTP mail error", e);
+		}
+	 }
+
+
+	/**
+	 * Sends an email notifying user that their registration has been accepted
+	 * 
+	 * @param mailTo - email address
+	 * @param userId - user's login ID
+	 * @throws Exception
+	 */
+	public static void sendConfirmationMail(String mailTo, String userId) throws Exception {
+	
+		try {
+		    String[] params = {userId, MailConfig.getAppSupportNumber(), MailConfig.getTechSupportStartTime(), MailConfig.getTechSupportEndTime(),};
+			
+	        MessageFormat formatter = new MessageFormat(MailConfig.getRegisterUnformattedBody());
+	        String formattedBody = formatter.format(params);		  
+				   
+	        new SendMail().sendMail(mailTo, null, formattedBody, formatFTPSubject());
+		} catch (Exception e) {
+			logger.error("Send confirmation mail error", e);
+		} catch (ValidationException e) {
+			logger.error("Send confirmation mail error", e);
+		}			
+     }
+	
+
+	/**
+	 * The filename is sometimes sent in with a leading slash.  Clean it off.
+	 * 
+	 * @param fileName
+	 * @return
+	 */
+	private static String cleanFileName(String fileName)
+	{
+		String nameWithoutPath;
+		if(fileName.lastIndexOf(File.separator)>0){
+			// Strip off path to just get the filename
+			nameWithoutPath = fileName.substring(fileName.lastIndexOf(File.separator)); 		
+		}
+		else{
+			nameWithoutPath = fileName; 		
+		}
+		return (nameWithoutPath.startsWith(File.separator)) ? nameWithoutPath.substring(1) : nameWithoutPath;
+	}
+	public static String formatFromAddress(){
+		 String from = new MessageFormat(MailConfig.getFrom()).format(new String[] {MailConfig.getAcronym()});
+		 return from;
+	}
+	public static String formatFTPSubject(){
+		 String ftpSubject = new MessageFormat(MailConfig.getFtpSubject()).format(new String[] {MailConfig.getProject(),MailConfig.getAcronym()});
+		 return ftpSubject;
+	}
+	public static String formatFTPErrorSubject(){
+		 String ftpSubject = new MessageFormat(MailConfig.getFtpErrorSubject()).format(new String[] {MailConfig.getProject(),MailConfig.getAcronym()});
+		 return ftpSubject;
+	}
+
+}
---
+++ QA_26SEP06_001/src/gov/nih/nci/caintegrator/application/test/TestCorrelationAnalysisRequest.java
@@ -0,0 +1,183 @@
+package gov.nih.nci.caintegrator.application.test;
+
+import gov.nih.nci.caintegrator.analysis.messaging.CorrelationRequest;
+import gov.nih.nci.caintegrator.analysis.messaging.DoubleVector;
+import gov.nih.nci.caintegrator.application.analysis.AnalysisServerClientManager;
+import gov.nih.nci.caintegrator.application.cache.CacheFactory;
+import gov.nih.nci.caintegrator.enumeration.CorrelationType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jms.JMSException;
+
+public class TestCorrelationAnalysisRequest {
+	
+	AnalysisServerClientManager analysisServerClientMgr;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestCorrelationAnalysisRequest tr = new TestCorrelationAnalysisRequest();
+		tr.initialize();
+		tr.sendRequest();
+
+	}
+	
+	public void sendRequest() {
+		
+		CorrelationRequest corrRequest = new CorrelationRequest("1","1");
+		
+		
+		double[] gx1 = {-0.331,
+						-1,
+						-0.392,
+						-1.008,
+						-1.849,
+						0.019,
+						-0.06,
+						0.429,
+						0.409,
+						1.194,
+						-1.103,
+						1.288,
+						-0.755,
+						-0.18,
+						-0.836,
+						0.906,
+						1.434,
+						0.067,
+						0.466,
+						0.133,
+						0.365,
+						1.091,
+						-0.949,
+						-1.502,
+						-0.739,
+						-0.025,
+						1.38,
+						2.056,
+						-0.598,
+						1.219,
+						-0.256,
+						0.021,
+						-1.059,
+						-1.545,
+						0.583,
+						-0.469,
+						2.478,
+						1.146,
+						0.727,
+						-0.461,
+						0.107,
+						-0.325,
+						-0.033,
+						0.417};
+		
+		
+		double[] gx2 = 
+		{0,
+		-1.06,
+		-0.03,
+		-0.46,
+		-2,
+		0.01,
+		0.95,
+		1.48,
+		-0.77,
+		0.3,
+		0.16,
+		1.03,
+		0.61,
+		0.03,
+		-1.5,
+		0.36,
+		3.17,
+		-0.89,
+		0.42,
+		1.41,
+		-0.62,
+		2.57,
+		-1.13,
+		-0.92,
+		-0.66,
+		-0.5,
+		3.48,
+		2.71,
+		-0.22,
+		0.94,
+		1.56,
+		0.56,
+		-0.88,
+		0.28,
+		0.73,
+		0.23,
+		1.55,
+		1.04,
+		0.94,
+		-0.9,
+		0.28,
+		0.36,
+		-1.81,
+		0.76};
+
+	
+		
+	    List<Double> gx1List = new ArrayList<Double>();
+	    List<Double> gx2List = new ArrayList<Double>();
+	    
+	    for (int i=0;i<gx1.length;i++) {
+	      gx1List.add(gx1[i]);
+	    }
+	    
+	    for (int j=0; j<gx2.length;j++) {
+	      gx2List.add(gx2[j]);
+	    }
+
+	    DoubleVector v1 = new DoubleVector("GX1", gx1List);
+	    DoubleVector v2 = new DoubleVector("GX2", gx2List);
+	    
+	    
+		corrRequest.setVector1(v1);
+		corrRequest.setVector2(v2);
+		
+		corrRequest.setCorrelationType(CorrelationType.PEARSON);
+		
+		try {
+			analysisServerClientMgr.sendRequest(corrRequest);
+		} catch (JMSException e) {
+			e.printStackTrace(System.out);
+		}
+		
+	}
+	
+	public void initialize() {
+		
+		
+		try {
+			analysisServerClientMgr = AnalysisServerClientManager.getInstance();
+			analysisServerClientMgr.setCache(CacheFactory.getBusinessTierCache());
+			
+			String jmsProviderURL = "localhost:1099";
+			String jndiFactoryName = "ConnectionFactory";
+			String requestQueueName = "queue/SharedAnalysisRequest";
+			String responseQueueName = "queue/ISPYanalysisResponse";
+			   
+			 analysisServerClientMgr.setJMSparameters(jmsProviderURL, jndiFactoryName, requestQueueName, responseQueueName);
+			   
+			 analysisServerClientMgr.establishQueueConnection();
+		
+			 System.out.println("Successfully established connection with JMS provider");
+			 
+			 
+			 
+		}
+		catch (Exception ex) {
+		  ex.printStackTrace(System.out);	
+		
+		}
+		
+	}
+
+}
