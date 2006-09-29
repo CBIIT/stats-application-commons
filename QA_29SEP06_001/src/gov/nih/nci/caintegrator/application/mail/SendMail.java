ef0b385643e267ee4f4b8a011966e3387df55e8c
no_author <no_author@no_author.cvs2svn>
2006-09-29 19:16:53 +0000

    This commit was manufactured by cvs2svn to create tag 'QA_29SEP06_001'.
    SVN-Revision: 8219

---
+++ QA_29SEP06_001/src/gov/nih/nci/caintegrator/application/mail/SendMail.java
@@ -0,0 +1,74 @@
+package gov.nih.nci.caintegrator.application.mail;
+
+
+import gov.nih.nci.caintegrator.exceptions.ValidationException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.InternetAddress;
+import javax.mail.Message;
+import javax.mail.Transport;
+
+import org.apache.commons.validator.EmailValidator;
+import org.apache.log4j.Logger;
+
+
+public class SendMail {
+	
+	private static Logger logger = Logger.getLogger(SendMail.class);
+	
+	public static String MAIL_PROPERTIES = "mail.properties"; 
+	 
+	 
+	
+	public SendMail(){
+	}
+	
+	public synchronized void sendMail(String mailTo, String mailCC, String mailBody, String subject )
+		throws ValidationException
+	{
+		try
+		{
+			if(mailTo != null && EmailValidator.getInstance().isValid(mailTo))
+			{
+				//get system properties
+				Properties props = System.getProperties();
+				
+				String to=mailTo;
+				// Set up mail server
+				
+				props.put("mail.smtp.host",MailConfig.getHost());
+				
+				//Get session
+				Session session = Session.getDefaultInstance(props, null);
+				
+				//Define Message
+				MimeMessage message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(MailManager.formatFromAddress()));
+				message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+				if ((mailCC != null) && EmailValidator.getInstance().isValid(mailCC))
+					message.addRecipient(Message.RecipientType.CC, new InternetAddress(mailCC));
+				message.setSubject(subject);
+			    message.setText(mailBody);
+			    
+				//Send Message
+				
+				Transport.send(message);
+			}
+			else
+			{
+				throw new ValidationException("Invalid Email Address");
+			}
+		}
+		catch (Exception e)
+		{
+			logger.error("Send Mail error", e);
+		}	//catch
+	}//send mail
+	
+}//Sendmail
+
+
