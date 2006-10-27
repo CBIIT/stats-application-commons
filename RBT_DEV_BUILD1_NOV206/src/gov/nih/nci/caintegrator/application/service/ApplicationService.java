62c839bc99222aa89ac318111d51b8ff80cf607b
no_author <no_author@no_author.cvs2svn>
2006-10-27 17:28:11 +0000

    This commit was manufactured by cvs2svn to create tag 'RBT_DEV_BUILD1_NOV206'.
    SVN-Revision: 8247

---
+++ RBT_DEV_BUILD1_NOV206/src/gov/nih/nci/caintegrator/application/service/ApplicationService.java
@@ -0,0 +1,12 @@
+package gov.nih.nci.caintegrator.application.service;
+
+/**
+ * All services available for an application will implement this interface
+ * @author harrismic
+ *
+ */
+public interface ApplicationService {
+
+	//public static ApplicationService getInstance() throws Exception;
+
+}
