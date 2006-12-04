b7934e02fe82dc167e0ee64a42f4ab08164063e3
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2006-12-04 11:08:15 +0000

    fixed security manager
    SVN-Revision: 8269

--- src/gov/nih/nci/caintegrator/security/SecurityManager.java
+++ src/gov/nih/nci/caintegrator/security/SecurityManager.java
@@ -89,6 +89,7 @@ import org.apache.log4j.Logger;
 */
 
 public class SecurityManager {
+	public static final String USER_NOT_FOUND_IN_AUTHENTICATION_DB = "USER NOT FOUND_IN_AUTHENTICATION_DB";
 	private static SecurityManager instance;
 	private static Logger logger = Logger.getLogger(SecurityManager.class);
 	private static AuthorizationManager authorizationManager;
@@ -110,13 +111,33 @@ public class SecurityManager {
 		}
 		return instance;
 	}
-
-	/***
-	 * Will authenticate the user by the password they provide. Will either 
-	 * create and return a populated UserCredentials object or will throw an
+	/**
+	 * 	
+	 * This method Will authenticate the user by the password they provide. Will either 
+	 *  return true, false or will throw an
 	 * AuthenticationException.
 	 * 
 	 * This implementation is currently using CSM via LDAP to authenticate.
+	 * Authorization is done through the CSM tables with the application database 
+	 * @param userName
+	 * @param password
+	 * @return
+	 * @throws AuthenticationException
+	 */
+	public boolean authenticate(String userName, String password) throws AuthenticationException{
+		boolean authenticated;
+		try {
+		 authenticated = localAuthenticate(userName,password);
+		}catch(AuthenticationException ae) {
+				throw new AuthenticationException(ae.getMessage());
+		}
+		return authenticated;
+	}
+	/***
+	 * Will authorize the user by the password they provide. Will either 
+	 * create and return a populated UserCredentials object or will throw an
+	 * AuthenticationException.
+	 * 
 	 * Authorization is done through the Rembrandt database where user roles
 	 * are stored with associated Institutes.
 	 * 
@@ -127,100 +148,96 @@ public class SecurityManager {
 	 *  
 	 * @throws AuthenticationException
 	 */
-	public UserCredentials authenticate(String userName, String password) throws AuthenticationException{
+	@SuppressWarnings("unchecked")
+	public UserCredentials authorization(String userName) throws AuthenticationException{
 		UserCredentials credentials = null;
-		boolean authenticated;
-		try {
-		 authenticated = localAuthenticate(userName,password);
-		}catch(AuthenticationException ae) {
-			if("RBTuser".equals(userName)&&"RBTpass".equals(password)) {
-				authenticated = true;
-			}else {
-				throw new AuthenticationException(ae.getMessage());
-			}
-		}
 		try {
 			authorizationManager = getAuthorizationManager();
 			userProvisioningManager = getUserProvisioningManager();
 		}catch(Exception e) {
-			logger.debug(e);
+			logger.error(e);
+		}
+		Collection<InstitutionDE> institutes = new ArrayList<InstitutionDE>();
+		User user = authorizationManager.getUser(userName);
+		if(user == null){
+			logger.warn("User "+ userName +" not found") ;
+			throw new AuthenticationException(USER_NOT_FOUND_IN_AUTHENTICATION_DB);
+		}
+		/**
+		 * Protection Elements are the data sets that each institute has submitted
+		 * Each user is asigned first a username, then a group.  A group is assigned
+		 * a protection group, and a protection group is assigned a set of protection
+		 * elements.
+		 * 
+		 * For instance danielR is assigned to the SUPER_USER group.  The SUPER_USER
+		 * group has a Protection Group called SUPER_USER_DATA.  SUPER_USER_DATA
+		 * has all the applications Protection Elements assigned to it.  In other
+		 * words username danielR is able to access all protection elements in
+		 * the application, or rather, danielR is able to access all submitted
+		 * data from all institutes.
+		 * 
+		 * IMPORTANT!
+		 * For caIntegrator we refer to what the CSM calls a group as a Role.
+		 * This may change later but I thought I would put that down here so 
+		 * as to avoid confusion.  So, we will be grabbing the assigned group(s)
+		 * for the user and then assigning them a "Role" in the application
+		 * 
+		 * DBauer
+		 */
+		Set protectionElements;
+		String emailAddress = null;
+		String firstName = null;
+		String lastName = null;
+		Set<Group> groups;
+		try {
+			emailAddress = user.getEmailId();
+			firstName = user.getFirstName();
+			lastName = user.getLastName();
+			protectionElements = userProvisioningManager.getProtectionElementPrivilegeContextForUser(user.getUserId().toString());
+			groups = userProvisioningManager.getGroups(user.getUserId().toString());	
+		} catch (Exception e) {
+			logger.error("No ProtectionElementPrivlegeContexts found for user:"+ userName);
+			logger.error(e);
+			throw new AuthenticationException("ProtectionElementPrivlegeContexts are null");
+		}
+		if(protectionElements!=null) {
+			try {
+				/*
+				 * For all the protection elements allowed for reading
+				 * create institution domain elements and store into the
+				 * user credentials.
+				 */
+				for(Iterator i = protectionElements.iterator();i.hasNext();) {
+					ProtectionElementPrivilegeContext pepc = (ProtectionElementPrivilegeContext)i.next();
+					ProtectionElement pe = pepc.getProtectionElement();
+					Long instituteId = new Long(pe.getObjectId());
+					String name = pe.getProtectionElementName();
+					institutes.add(new InstitutionDE(name,instituteId));
+				}
+				
+				logger.debug("Username: "+userName+" has the following credentials:");
+				logger.debug("--------------------------------------------------------");
+				for(InstitutionDE institute:institutes) {
+					logger.debug("Allowed to read:" +institute.getInstituteName());
+				}
+				logger.debug("--------------------------------------------------------");
+				/*
+				 * Create the UserRole for the application.  Right now
+				 * this isn't really used for anything as the actual
+				 * credentials contain the information that will be used to
+				 * determine what the user is allowed to actually see.  But
+				 * we will pass it along for now incase we want to make any
+				 * snap judgments about the user after they are logged in
+				 */
+				UserRole role = getUserRole(groups);
+				credentials = new UserCredentials(emailAddress,firstName,institutes,lastName,role,userName);
+			}catch(NullPointerException npe) {
+				logger.error("Security Objects are null.") ;
+				throw new AuthenticationException("Some SecurityObjects are null");
+			}
 		}
 		
-		if(authenticated) {
-			Collection<InstitutionDE> institutes = new ArrayList<InstitutionDE>();
-			User user = authorizationManager.getUser(userName);
-			/**
-			 * Protection Elements are the data sets that each institute has submitted
-			 * Each user is asigned first a username, then a group.  A group is assigned
-			 * a protection group, and a protection group is assigned a set of protection
-			 * elements.
-			 * 
-			 * For instance danielR is assigned to the SUPER_USER group.  The SUPER_USER
-			 * group has a Protection Group called SUPER_USER_DATA.  SUPER_USER_DATA
-			 * has all the applications Protection Elements assigned to it.  In other
-			 * words username danielR is able to access all protection elements in
-			 * the application, or rather, danielR is able to access all submitted
-			 * data from all institutes.
-			 * 
-			 * IMPORTANT!
-			 * For caIntegrator we refer to what the CSM calls a group as a Role.
-			 * This may change later but I thought I would put that down here so 
-			 * as to avoid confusion.  So, we will be grabbing the assigned group(s)
-			 * for the user and then assigning them a "Role" in the application
-			 * 
-			 * DBauer
-			 */
-			Set protectionElements;
-			String emailAddress = user.getEmailId();;
-			String firstName = user.getFirstName();;
-			String lastName = user.getLastName();;
-			Set<Group> groups;
-			try {
-				protectionElements = userProvisioningManager.getProtectionElementPrivilegeContextForUser(user.getUserId().toString());
-				groups = userProvisioningManager.getGroups(user.getUserId().toString());	
-			} catch (Exception e) {
-				logger.error("No ProtectionElementPrivlegeContexts found for user:"+ userName);
-				logger.error(e);
-				throw new AuthenticationException("ProtectionElementPrivlegeContexts are null");
-			}
-			if(protectionElements!=null) {
-				try {
-					/*
-					 * For all the protection elements allowed for reading
-					 * create institution domain elements and store into the
-					 * user credentials.
-					 */
-					for(Iterator i = protectionElements.iterator();i.hasNext();) {
-						ProtectionElementPrivilegeContext pepc = (ProtectionElementPrivilegeContext)i.next();
-						ProtectionElement pe = pepc.getProtectionElement();
-						Long instituteId = new Long(pe.getObjectId());
-						String name = pe.getProtectionElementName();
-						institutes.add(new InstitutionDE(name,instituteId));
-					}
-					
-					logger.debug("Username: "+userName+" has the following credentials:");
-					logger.debug("--------------------------------------------------------");
-					for(InstitutionDE institute:institutes) {
-						logger.debug("Allowed to read:" +institute.getInstituteName());
-					}
-					logger.debug("--------------------------------------------------------");
-					/*
-					 * Create the UserRole for the application.  Right now
-					 * this isn't really used for anything as the actual
-					 * credentials contain the information that will be used to
-					 * determine what the user is allowed to actually see.  But
-					 * we will pass it along for now incase we want to make any
-					 * snap judgments about the user after they are logged in
-					 */
-					UserRole role = getUserRole(groups);
-					credentials = new UserCredentials(emailAddress,firstName,institutes,lastName,role,userName);
-				}catch(NullPointerException npe) {
-					logger.error("Security Objects are null.") ;
-					throw new AuthenticationException("Some SecurityObjects are null");
-				}
-			}
-			
-		}if(credentials==null){
+		if(credentials==null){
 			logger.error("authentication is returning a Null Credentials Object");
 		}
 		return credentials;
