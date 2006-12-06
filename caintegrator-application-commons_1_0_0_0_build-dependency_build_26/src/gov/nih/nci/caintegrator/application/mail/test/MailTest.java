04c207e649a8200cc2d67cd5e015477161a9acde
no_author <no_author@no_author.cvs2svn>
2006-12-06 21:17:16 +0000

    This commit was manufactured by cvs2svn to create tag 'caintegrator-application-commons_1_0_0_0_build-dependency_build_26'.
    SVN-Revision: 8290

---
+++ caintegrator-application-commons_1_0_0_0_build-dependency_build_26/src/gov/nih/nci/caintegrator/application/mail/test/MailTest.java
@@ -0,0 +1,41 @@
+package gov.nih.nci.caintegrator.application.mail.test;
+
+import gov.nih.nci.caintegrator.application.mail.MailManager;
+import junit.framework.TestCase;
+import java.util.ArrayList;
+
+public class MailTest extends TestCase {
+
+	public MailTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testMail() {
+		
+		 //MailManager mailMgr = new MailManager();
+		 //System.out.println(mailMgr);
+		 try {
+			 
+			 
+             ArrayList<String> al = new ArrayList<String>();
+             al.add("test.zip");
+            String mailProperties = "gov.nih.nci.cgems.mail.properties";
+            System.setProperty(mailProperties,"C:/local/content/cgems/cgems/config/mail.properties");
+			MailManager mailManager = new MailManager(mailProperties);
+
+			mailManager.sendFTPMail("sahnih@mail.nih.gov", al, null);
+			mailManager.sendFTPErrorMail("sahnih@mail.nih.gov");
+		 } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+	}
+
+}
