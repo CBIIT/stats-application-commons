b0d34abb4569f801a9165a4f8a5b078c25460702
no_author <no_author@no_author.cvs2svn>
2006-12-06 21:17:12 +0000

    This commit was manufactured by cvs2svn to create tag 'ISPY_VER1P0_RC7_13DEC06'.
    SVN-Revision: 8286

---
+++ ISPY_VER1P0_RC7_13DEC06/src/gov/nih/nci/caintegrator/application/test/TestCompoundAnalysisRequest.java
@@ -0,0 +1,13 @@
+package gov.nih.nci.caintegrator.application.test;
+
+public class TestCompoundAnalysisRequest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
