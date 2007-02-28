474f1e88bbb43cddeb6b1c1d793faa563d866c37
no_author <no_author@no_author.cvs2svn>
2007-02-28 16:30:10 +0000

    This commit was manufactured by cvs2svn to create tag 'QA_CGEMS_28FEB07_01'.
    SVN-Revision: 8357

---
+++ QA_CGEMS_28FEB07_01/src/gov/nih/nci/caintegrator/application/configuration/SpringContext.java
@@ -0,0 +1,47 @@
+package gov.nih.nci.caintegrator.application.configuration;
+
+import java.io.Serializable;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+/**
+ * Helper class that provides an easy way to load beans from any point in the application,
+ * not just from a spring managed bean.
+ * 
+ *
+ * @author caIntegrator Team
+ */
+public class SpringContext implements Serializable {
+    
+    private static final long serialVersionUID = 3125223827888524892L;
+         
+    private static WebApplicationContext wac;
+         
+    private static boolean initialized = false;
+      
+    private SpringContext() {}
+     
+    protected static synchronized WebApplicationContext initialize(final ServletContext servletContext) {
+      if (wac == null) {
+        wac = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
+        initialized = true;
+      }
+      return wac;
+    }
+    
+    /**
+     * Returns a bean from the spring ApplicationFactory with the
+     * provided name.
+     * 
+     * @param name
+     * @return
+     * @throws IllegalStateException
+     */
+    public static Object getBean(final String name) throws IllegalStateException {
+      if (!initialized) throw new IllegalStateException("SpringContext has not been initialized.");
+      return wac.getBean(name);
+    }
+  }
