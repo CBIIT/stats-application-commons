86b73b109f0db4344a044e539dafaaefec328e2b
Andrew Shinohara <Andrew.Shinohara@fda.hhs.gov>
2007-05-30 19:02:56 +0000

    logging query exceptions
    SVN-Revision: 8422

--- src/gov/nih/nci/caintegrator/application/service/strategy/AsynchronousFindingStrategy.java
+++ src/gov/nih/nci/caintegrator/application/service/strategy/AsynchronousFindingStrategy.java
@@ -14,6 +14,7 @@ import gov.nih.nci.caintegrator.studyQueryService.QueryHandler;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.core.task.TaskExecutor;
 
 /**
@@ -34,7 +35,7 @@ public class AsynchronousFindingStrategy extends SessionBasedFindingStrategy {
     // of "domain findings" from the executed query.  This will be
     // placed into the BusinessTierCache
     protected Finding applicationFinding;
-    
+    private static Logger logger = Logger.getLogger(AsynchronousFindingStrategy.class);    
     protected TaskExecutor taskExecutor;
     
     
@@ -161,6 +162,7 @@ public class AsynchronousFindingStrategy extends SessionBasedFindingStrategy {
                 getTaskResult().setTaskResults(findings);
                 getTaskResult().getTask().setStatus(FindingStatus.Completed);
                 } catch(Exception e) {
+                    logger.error("Error issuing query", e);
                     FindingStatus status = FindingStatus.Error;
                     status.setComment(e.getMessage());
                     getTaskResult().getTask().setStatus(status);
