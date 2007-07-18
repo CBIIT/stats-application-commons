ee823e0301f999c054c456531a962f235ae7d1ce
no_author <no_author@no_author.cvs2svn>
2007-07-18 20:47:02 +0000

    This commit was manufactured by cvs2svn to create tag 'ISPY_QA_31JULY2007'.
    SVN-Revision: 8477

---
+++ ISPY_QA_31JULY2007/src/gov/nih/nci/caintegrator/application/util/StringUtils.java
@@ -0,0 +1,54 @@
+package gov.nih.nci.caintegrator.application.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class StringUtils {
+
+	/**
+	 * This method will extract tokens from a string containing multiple tokens 
+	 * and place the tokens in a collection.  This is used for example, to parse strings containing multiple GO or PATHWAY ids.
+	 * @param tokenCollection
+	 * @param tokenStr
+	 * @param delimeter
+	 */
+	public static List<String> extractTokens(String text, String delim) {
+	  List<String> tokenList = new ArrayList<String>();
+	  if (text != null) {
+	    String[] tokens = text.split(delim);
+	  
+	    for (int i=0; i < tokens.length; i++) {
+	      tokenList.add(tokens[i].trim());
+	    }
+	  }
+	  return tokenList;
+	}
+	
+	public static String concatinateStrings(List<String> strList, String delim) {
+	  StringBuffer sb = new StringBuffer();
+	  for (Iterator i=strList.iterator(); i.hasNext(); ) {
+	     sb.append(i.next());
+	     if (i.hasNext()) sb.append(delim);
+	  }
+	  return sb.toString();
+	}
+	
+	public static boolean isEmptyStr(String str) {
+	  return ((str==null)||(str.trim().length()==0));
+	}
+	
+	/**
+	 * This method will get the double represented by the specified string
+	 * If the input string is empty or null then null will be returned.
+	 * @param doubleStr
+	 * @return
+	 */
+	public static Double getDouble(String doubleStr) {
+	  if (!StringUtils.isEmptyStr(doubleStr)) {
+	    return Double.valueOf(doubleStr);
+	  }
+	  return null;
+	}
+
+}
