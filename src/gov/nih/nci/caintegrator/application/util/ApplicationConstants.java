f6b9b08a9b1e2dd117f0a53f16417a401a13fc75
Harris Michael <harrismic>
2007-08-08 13:29:16 +0000

    Common application constants.
    SVN-Revision: 8488

---
+++ src/gov/nih/nci/caintegrator/application/util/ApplicationConstants.java
@@ -0,0 +1,6 @@
+package gov.nih.nci.caintegrator.application.util;
+
+public class ApplicationConstants {
+	 public static final String userInfoBean = "userInfoBean";
+	 public static final String loggedIn = "loggedIn";
+}
