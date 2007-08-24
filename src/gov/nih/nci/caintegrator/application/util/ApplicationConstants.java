9410c6554542e08b92df3d16e03a5e75d5e28001
Kevin Rosso <Kevin.Rosso@fda.hhs.gov>
2007-08-24 20:10:00 +0000

    default login service addition to constants
    SVN-Revision: 8494

--- src/gov/nih/nci/caintegrator/application/util/ApplicationConstants.java
+++ src/gov/nih/nci/caintegrator/application/util/ApplicationConstants.java
@@ -2,5 +2,6 @@ package gov.nih.nci.caintegrator.application.util;
 
 public class ApplicationConstants {
 	 public static final String userInfoBean = "userInfoBean";
+     public static final String SHELL_APP = "SHELL_APP";
 	 public static final String loggedIn = "loggedIn";
 }
