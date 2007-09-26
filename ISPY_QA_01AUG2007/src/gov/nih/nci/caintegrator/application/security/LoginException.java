875595f53d1977480475cd1daac4368b6a0ae04a
no_author <no_author@no_author.cvs2svn>
2007-09-26 15:51:07 +0000

    This commit was manufactured by cvs2svn to create tag 'ISPY_QA_01AUG2007'.
    SVN-Revision: 8529

---
+++ ISPY_QA_01AUG2007/src/gov/nih/nci/caintegrator/application/security/LoginException.java
@@ -0,0 +1,10 @@
+package gov.nih.nci.caintegrator.application.security;
+
+public class LoginException extends Exception {
+
+    public LoginException() {
+    }
+    public LoginException(Throwable e) {
+        super(e);
+    }
+}
