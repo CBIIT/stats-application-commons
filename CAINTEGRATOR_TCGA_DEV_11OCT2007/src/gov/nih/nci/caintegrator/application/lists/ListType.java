a301311ab2d79b6a8b92042a4cb13b0b188b6f60
no_author <no_author@no_author.cvs2svn>
2007-10-11 14:16:30 +0000

    This commit was manufactured by cvs2svn to create tag 'CAINTEGRATOR_TCGA_DEV_11OCT2007'.
    SVN-Revision: 8539

---
+++ CAINTEGRATOR_TCGA_DEV_11OCT2007/src/gov/nih/nci/caintegrator/application/lists/ListType.java
@@ -0,0 +1,12 @@
+/**
+ * enum ListType defines the objects in the user's lists
+ */
+package gov.nih.nci.caintegrator.application.lists;
+
+/**
+ * @author rossok
+ *
+ */
+public enum ListType {
+    PatientDID, Gene, Reporter;
+}
