0da92d509acac42b5f1e364f7e76d3854030f11b
svnuser <svnuser>
2009-05-26 22:06:23 +0000

    Label Created For Anthill Pro
    SVN-Revision: 10247

---
+++ caintegrator-application-commons_1_0_0_0_build-dependency_build_14685/src/gov/nih/nci/caintegrator/application/bean/IHCFindingReportBean.java
@@ -0,0 +1,10 @@
+package gov.nih.nci.caintegrator.application.bean;
+
+public interface IHCFindingReportBean {
+
+    public String getPatientDID();
+    
+    public String getTimepoint();
+    
+    public String getSpecimenIdentifier();
+}
