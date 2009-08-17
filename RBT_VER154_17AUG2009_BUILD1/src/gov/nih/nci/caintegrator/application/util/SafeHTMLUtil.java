5cbe46dadd5109ed954b8ecf372e8d3c85ac3b76
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2009-08-17 15:52:45 +0000

    SVN-Revision: 10374

---
+++ RBT_VER154_17AUG2009_BUILD1/src/gov/nih/nci/caintegrator/application/util/SafeHTMLUtil.java
@@ -0,0 +1,30 @@
+package gov.nih.nci.caintegrator.application.util;
+
+import org.apache.commons.lang.StringUtils;
+import org.htmlparser.util.Translate;
+
+public class SafeHTMLUtil {
+
+	public static String clean(String s) {
+		String clean = Translate.decode(s).replace("<", "").replace(">", "");
+		if(!clean.contains("description") && !clean.contains("Description") ){
+		clean = StringUtils.replace(clean, "script", "");
+		}
+		clean = StringUtils.replace(clean, "%", "");
+		clean = StringUtils.replace(clean, "#", "");
+		clean = StringUtils.replace(clean, ";", "");
+		clean = StringUtils.replace(clean, "'", "");
+		clean = StringUtils.replace(clean, "\"", "");
+		clean = StringUtils.replace(clean, "$", "");
+		clean = StringUtils.replace(clean, "&", "");
+		clean = StringUtils.replace(clean, "(", "");
+		clean = StringUtils.replace(clean, ")", "");
+		clean = StringUtils.replace(clean, "/", "");
+		clean = StringUtils.replace(clean, "\\", "");
+		if (clean.length() == 0) {
+			clean = "unamedQuery";
+		}
+		return clean;
+
+	}
+}
