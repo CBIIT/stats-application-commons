d9b66ef7c97cfedeab300823df384755b6d0442f
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2010-03-12 03:09:18 +0000

    added method to generate unique name
    SVN-Revision: 10596

--- src/gov/nih/nci/caintegrator/application/zip/FileNameGenerator.java
+++ src/gov/nih/nci/caintegrator/application/zip/FileNameGenerator.java
@@ -14,7 +14,25 @@ import org.apache.commons.validator.EmailValidator;
  *
  */
 public class FileNameGenerator {
-    public static String generateFileName(String userEmail) throws ValidationException{
+    public static String generateFileName(String userName) throws ValidationException{
+    	String fileName = null;
+    	if(userName != null ){
+	    	// Get a random number of up to 7 digits   	
+	    	int sevenDigitRandom = new Double(Math.random() * 10000000).intValue();
+	    	
+	    	// Get the last 7 digits of the Java timestamp
+	    	String s = String.valueOf(System.currentTimeMillis());
+	    	String lastSevenOfTimeStamp = s.substring(s.length()-7, s.length());
+	    	
+	    	// Put it all together
+	        fileName = userName + "_" + sevenDigitRandom + lastSevenOfTimeStamp;
+    	}
+    	else{
+    		throw new ValidationException("Invalid Email Address");
+    	}
+        return fileName;
+    }
+    public static String generateFileNameFromEmail(String userEmail) throws ValidationException{
     	String fileName = null;
     	if(userEmail != null && EmailValidator.getInstance().isValid(userEmail)){
 	    	// Get a random number of up to 7 digits   	
